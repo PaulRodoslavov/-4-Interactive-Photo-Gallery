5828181948928b2f5ccd1fa86c70825a8002a109
PaulRodoslavov <pasha.rodoslavov@gmail.com>
2018-05-02 22:45:34 -0700

    fixed plugin

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log($(".container"));
+lightbox.option({
+      // 'albumLabel': "",
+      'wrapAround': true,
+      'alwaysShowNavOnTouchDevices': true,
+      'showImageNumberLabel': false
+    })
