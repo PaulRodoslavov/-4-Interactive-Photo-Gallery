4d417af2b8f6b623abc2c66c878351810bfaab88
PaulRodoslavov <pasha.rodoslavov@gmail.com>
2018-05-03 22:53:11 -0700

    updated

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+
+/* Settings fot Lightbox plugin ------------ */
+
 lightbox.option({      
       'wrapAround': true,
       'alwaysShowNavOnTouchDevices': true,
       'showImageNumberLabel': false
     });
 
+/* Creating titles under pictures ------------ */
 
 const atr = $('img');
 let div = $("<div>").addClass('olo');
@@ -13,11 +17,11 @@ let div = $("<div>").addClass('olo');
 const text = $('.olo');	 
 
 	for(let i = 0; i < atr.length; i++) {
-		console.log(atr[i].getAttribute('alt'));	
 		let getAtr = atr[i].getAttribute('alt');
 			text[i].innerHTML = getAtr;
 		}
 
+/* Function for instant search ------------ */
 
 $( "#search_words" ).keyup(function() {
  	const search = $( "#search_words" ).val().toLowerCase();
